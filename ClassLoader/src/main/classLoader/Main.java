a20a2af942be82cc8d3d2ad9e3ac399cfd5e5b9c
Andryxa <Andryxa97@gmail.com>
2021-11-09 17:50:23 +0300

    Corrected commit

--- ClassLoader/src/main/classLoader/Main.java
+++ ClassLoader/src/main/classLoader/Main.java
@@ -6,17 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
-                Choose witch class load.
+                Choose which class load.
                 If you enter 1, will load class Hello.
                 If you enter 2, will load class Loop.""");
         int num = scanner.nextInt();
-        if (num == 1) Hello.say();
-        else if (num == 2) {
-            System.out.println("Inter size of loop");
+        if (num == 1) {
+            Hello.say();
+        } else if (num == 2) {
+            System.out.println("Enter size of loop");
             int size = scanner.nextInt();
             Loop.start(size);
         } else {
-            System.out.println("no class found");
+            System.out.println("No class found");
         }
     }
 
