854caa960499986ee18466c63cc2d280bdb65cd1
Andryxa <Andryxa97@gmail.com>
2021-11-10 17:56:19 +0300

    SQL and noSQL 1 commit

--- SQL_NoSQL/src/main/java/com/sql/ConnectionToNoSQL.java
+++ SQL_NoSQL/src/main/java/com/sql/ConnectionToNoSQL.java
@@ -4,6 +4,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import java.util.Map;
@@ -15,7 +16,7 @@ public class ConnectionToNoSQL {
     private static Scanner scanner = new Scanner(System.in);
     private static MongoCollection<Document> collection;
 
-    public static void start() {
+    public void start() {
         try {
             try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
                 MongoDatabase database = mongoClient.getDatabase("library");
@@ -32,10 +33,11 @@ public class ConnectionToNoSQL {
 
                 if (coll == 1) {
                     collection = database.getCollection("users");
-                    System.out.println("If you want to show all users press 1 \n" +
-                            "If you want to add new user press 2 \n" +
-                            "If you want to delete user press 3 \n" +
-                            "If you want search user by name press 4");
+                    System.out.println("""
+                            If you want to show all users press 1\s
+                            If you want to add new user press 2\s
+                            If you want to delete user press 3\s
+                            If you want search user by name press 4""");
                     int method = scanner.nextInt();
 
                     if (method == 1) {
@@ -55,7 +57,7 @@ public class ConnectionToNoSQL {
                     if (method == 1) {
                         showAll();
                     } else if (method == 2) {
-
+                        availableBooks();
                     }
                 }
             }
@@ -88,6 +90,11 @@ public class ConnectionToNoSQL {
         System.out.println("Enter name");
         String name = scanner.next();
         collection.find(new Document("name", new Document("$regex", name)))
+                .forEach((Consumer<Document>) System.out::println);
+    }
+
+    private static void availableBooks() {
+        collection.find(Filters.eq("available", true))
                 .forEach((Consumer<Document>) System.out::println);
     }
 }
