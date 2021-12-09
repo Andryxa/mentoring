e39c29d79a9924630af773d16f670a467bd0f79b
Andryxa <Andryxa97@gmail.com>
2021-12-09 18:31:49 +0300

    Hibernate main fixed

--- Hibernate/src/main/java/com/andryxa/mentoring/Main.java
+++ Hibernate/src/main/java/com/andryxa/mentoring/Main.java
@@ -17,25 +17,25 @@ public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final String unitName = "default";
+        final ClientFunctional clientFunctional = new ClientFunctionalImpl(unitName);
+        final ClientMenu clientMenu = new ClientMenu(scanner, clientFunctional);
+        final AccountFunctional accountFunctional = new AccountFunctionalImpl(unitName);
+        final AccountMenu accountMenu = new AccountMenu(scanner, accountFunctional);
+        final ClientsAndAccounts clientsAndAccounts = new ClientsAndAccountsImpl(unitName);
+        final ClientsAndAccountsMenu clientsAndAccountsMenu = new ClientsAndAccountsMenu(clientsAndAccounts);
 
         while (true) {
-            System.out.println("If you want to work with table 'clients' press 1\n" +
+            System.out.println("\nIf you want to work with table 'clients' press 1\n" +
                     "If you want to work with table 'accounts' press 2\n" +
                     "If you want to see all clients and their accounts press 3\n" +
                     "If you want to close the program press 0");
             int choice = scanner.nextInt();
 
             if (choice == 1) {
-                final ClientFunctional clientFunctional = new ClientFunctionalImpl(unitName);
-                final ClientMenu clientMenu = new ClientMenu(scanner, clientFunctional);
                 clientMenu.start();
             } else if (choice == 2) {
-                final AccountFunctional accountFunctional = new AccountFunctionalImpl(unitName);
-                final AccountMenu accountMenu = new AccountMenu(scanner, accountFunctional);
                 accountMenu.start();
             } else if (choice == 3) {
-                final ClientsAndAccounts clientsAndAccounts = new ClientsAndAccountsImpl(unitName);
-                final ClientsAndAccountsMenu clientsAndAccountsMenu = new ClientsAndAccountsMenu(clientsAndAccounts);
                 clientsAndAccountsMenu.start();
             } else if (choice == 0) {
                 break;
